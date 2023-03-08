4254c195cf10b2e53a80a5d9235e2c94e254fa64
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-03-07 20:29:59 -0500

    installed react router and modified app.js accordingly to make new pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ProductList from "./pages/ProductList";
 
 function App() {
-  return <Home />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/products" element={<ProductList />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ src/pages/ProductList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Productlist() {
+  return (
+    <Container>Productlist</Container>
+  )
+}
+
+const Container = styled.div``
+
+export default Productlist
