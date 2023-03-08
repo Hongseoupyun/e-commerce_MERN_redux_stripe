e2331f6f61020cf1b743cd2911c7ac071ba206bb
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-03-07 21:02:41 -0500

    developed(ProductList):developed ProductList component

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,12 +1,83 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import Navbar from "components/Navbar";
+import Announcement from "components/Announcement";
+import Products from "components/Products";
+import Footer from "components/Footer";
+import Newsletter from "components/Newsletter";
 
 function Productlist() {
   return (
-    <Container>Productlist</Container>
-  )
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Title>Products</Title>
+      <FilterContainer>
+        <Filter>
+          <FilterText>Filter Products:</FilterText>
+          <Select>
+            <Option disabled selected>
+              Color
+            </Option>
+            <Option>White</Option>
+            <Option>Black</Option>
+            <Option>Red</Option>
+            <Option>Blue</Option>
+            <Option>Yellow</Option>
+            <Option>Green</Option>
+          </Select>
+          <Select>
+            <Option disabled selected>
+              Size
+            </Option>
+            <Option>XS</Option>
+            <Option>S</Option>
+            <Option>M</Option>
+            <Option>L</Option>
+            <Option>XL</Option>
+            <Option>XXL</Option>
+          </Select>
+        </Filter>
+        <Filter>
+          <FilterText>Sort Products:</FilterText>
+          <Select>
+            <Option>Sort by: Low Price</Option>
+            <Option>Sort by: High Price</Option>
+          </Select>
+        </Filter>
+      </FilterContainer>
+      <Products />
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
 }
 
-const Container = styled.div``
+const Container = styled.div``;
+const Title = styled.h1`
+  margin: 20px;
+`;
+const FilterContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0px 20px;
+`;
+const Filter = styled.div`
+  margin: 20px;
+`;
 
-export default Productlist
+const FilterText = styled.span`
+  font-size: 20px;
+  font-weight: 600;
+`;
+
+const Select = styled.select`
+  padding: 5px;
+  margin-left: 10px;
+  border: 1px solid lightgray;
+
+`;
+const Option = styled.option``;
+
+export default Productlist;
