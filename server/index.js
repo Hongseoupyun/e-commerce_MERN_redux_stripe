c446ad2ebb81465e399428ca16be218982b228b5
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-03-09 19:53:00 -0500

    developed(mongoDB/dotenv):connected to mongoDB using mongoose and used dotenv for security

--- server/index.js
+++ server/index.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 
-app.listen(3001, () => {
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log("Connection failed and error occured", error);
+  });
+
+app.listen(process.env.PORT || 3001, () => {
   console.log("Server is running on port 3001");
 });
