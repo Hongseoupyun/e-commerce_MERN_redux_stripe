10733da8c553d3ec17202d7f24a726cbd32b7c29
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-03-18 19:42:19 -0400

    developed(api/auth/signup):added encryption for password

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
 const User = require("../models/User");
+const CryptoJS = require("crypto-js");
 
 //register
-router.post("signup", async (req, res) => {
+router.post("/signup", async (req, res) => {
   const { username, email, password } = req.body;
   const newUser = new User({
     username,
     email,
-    password,
+    password: CryptoJS.AES.encrypt(password, process.env.SECRET_PHARASE).toString(),
   });
   try {
     const savedUser = await newUser.save();
