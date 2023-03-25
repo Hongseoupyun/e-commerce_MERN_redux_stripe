20bc45cfae699b939fa835fcda8966c72f9e332a
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2023-03-25 01:15:31 -0400

    developed(api/users/ and api/users/find:id):developed logic for users api to delete users info and get users infos as an admin user

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,9 @@
 const router = require("express").Router();
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const {
+  verifyTokenAndAuthorization,
+  verifyTokenAndIsAdmin,
+} = require("./verifyToken");
 const CryptoJS = require("crypto-js");
 
 //Update user information
@@ -28,4 +31,39 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   }
 });
 
+// Delete user
+
+router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    // Delete the user using the provided id
+    await User.findByIdAndDelete(req.params.id);
+    res.status(200).json("User has been deleted");
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Get a single user info as an admin
+router.get("/find/:id", verifyTokenAndIsAdmin, async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password: _, ...others } = user._doc;
+    res.status(200).json(others);
+  } catch {
+    res.status(500).json(err);
+  }
+});
+
+// Get all users as an admin
+router.get("/", verifyTokenAndIsAdmin, async (req, res) => {
+  const query = req.query.new;
+  try {
+    const allUsers = query
+      ? await User.find().sort({ _id: -1 }).limit(10)
+      : await User.find();
+    res.status(200).json(allUsers);
+  } catch {
+    res.status(500).json(err);
+  }
+});
 module.exports = router;
